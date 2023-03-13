5cce037307936a6600fc3cb578887d98294e8880
jumyeong33 <jm01028250379@gmail.com>
2023-03-13 12:40:59 +0900

    [Modify] finish optimal
    edit prompt question
    generate answer from openai with prompt

--- src/optimalCandidate.js
+++ src/optimalCandidate.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 const fs = require('fs');
 const basePath = process.cwd();
-const { errorMsg, sendMsg, system } = require('../lib/pretty');
+const { errorMsg, sendMsg, system, yellow } = require('../lib/pretty');
 const { generateResponse } = require('../lib/openAi');
 const readline = require('readline');
 
@@ -32,25 +32,31 @@ function readFileFromDir() {
 function readFileAndMakePrompt(jobGenerated) {
     let properBasePath = properPath();
     let index = 1;
-    let prompt = `which candidate resume is the most fit when compared the job requirements :
+    let prompt = `Are there optimal candidates according to job requirements?, if no, say 'plz give me other resume'. or if yes, pick one candidate and tell me about reason comparing and match with job requirements.
+    ===
     Job requirements : 
     ${jobGenerated}
     ===`;
     try {
         const files = fs.readdirSync(`${properBasePath}/assets/candidates`);
         for(const file of files) {
-            const candidate = readFile(`${properBasePath}/assets/job/${file}`);
+            const candidate = readFile(`${properBasePath}/assets/candidates/${file}`);
             const tempPrompt = `
             Candidate ${index} : 
             ${candidate}
             ===
             `
             prompt = prompt + tempPrompt;
+            console.log(`Add candidate resume ${index}...`)
             index++;
+        }
+        if(index === 2) {
+          console.log(errorMsg('Plz add two resumes at least..'));
+          process.exit(1);
         }
         return prompt;
     } catch(err) {
-        console.log(errorMsg('Cannot make propmt based on resumes'));
+        console.log(errorMsg('Cannot make propmt based on resumes.. plz add a resume'));
         process.exit(1);
     }
 
@@ -69,12 +75,18 @@ async function promptOptimal() {
     try{
       console.log(system('Load.....'));
       jobGenerated = await generateResponse(jobPromt);
+      console.log(yellow('Job ad Summary'));
       console.log(jobGenerated);
     } catch (err) {
       console.log(err);
     }
     rl.question(sendMsg('Press enter to Start >>'), async function() {
-        readFileAndMakePrompt(jobGenerated)
+        const prompt = readFileAndMakePrompt(jobGenerated);
+        const result = await generateResponse(prompt);
+        console.log(result);
+        console.log(sendMsg('bye'));
+        rl.close();
     })
 }
+
 promptOptimal();
